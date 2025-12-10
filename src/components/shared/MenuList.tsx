a0d26d667a23d7b30bffbe6f0cb36f64c50750ba
Arun Vijayarengan <arunprabu.v@hotmail.com>
2025-12-10 14:45:34 +0530

    refactor: replace Link with NavLink for improved navigation

--- src/components/shared/MenuList.tsx
+++ src/components/shared/MenuList.tsx
@@ -1,31 +1,33 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 // Functional Component with Arrow Function
 const MenuList = () => {
-  return(
-    <ul className="navbar-nav me-auto mb-2 mb-md-0">
-      <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/netflix">Netflix</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/employees">Employees</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/todos-version1">Todos using useState</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/todos-version2">Todos using useReducer</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/spotify">Spotify</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about-us">About Us</Link>
-      </li>
-    </ul>
+  return (
+    <nav aria-label="Main navigation">
+      <ul className="navbar-nav me-auto mb-2 mb-md-0">
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/netflix">Netflix</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/employees">Employees</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/todos-version2">Todos using useReducer</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/spotify">Spotify</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/notes">Notes</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link" to="/about-us">About Us</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
